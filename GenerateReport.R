15c35f8851acc4bf232ac6754dffe0038138fb54
llmorreale <llm77@cornell.edu>
2016-12-09 14:12:55 -0500

    added parameters to YAML and render function

--- GenerateReport.R
+++ GenerateReport.R
@@ -6,4 +6,4 @@ Scenario <- "CC"
 ref <- read.csv("scenario_reference.csv")
 scen_param <- ref[ref$scenario_init == Scenario,]
 
-rmarkdown::render("testdoc.Rmd", params = list(report_name = scen_param$scenario_name))
+rmarkdown::render("testdoc.Rmd", params = list(report_name = scen_param$scenario_name, scen_overview = scen_param$overview ))
